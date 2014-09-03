cb0ac395d70dd68827dbd66ece4bcc77282e023b
Salvador Gironès <salvadorgirones@gmail.com>
2014-09-03 10:18:18 -0700

    foo

--- lib/builder.go
+++ lib/builder.go
@@ -248,8 +248,7 @@ func (b *Builder) prepareBuilderEnv() error {
 
 func (b *Builder) SaveImage(p *Package, img *Image) error {
 	// Export container as an image
-	cmd := fmt.Sprintf("docker save %v -o %v", img.FullName(), filepath.Join(p.Path, img.Path))
-	log.Println(cmd)
+	cmd := fmt.Sprintf("docker save -o %v %v", filepath.Join(p.Path, img.Path), img.FullName())
 	c := exec.Command("bash", "-c", cmd)
 	if out, err := c.CombinedOutput(); err != nil {
 		return errors.New("Error saving " + img.FullName() + ": " + string(out))
